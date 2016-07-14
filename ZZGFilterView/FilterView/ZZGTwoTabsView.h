6d8383fc75200771617ae7f2a6320fa834b2d66c
zouzhigang <zouzhigang@loopeer.com>
2016-07-14 16:58:29 +0800

    publish ZZGFilterView to githb

---
+++ ZZGFilterView/FilterView/ZZGTwoTabsView.h
@@ -0,0 +1,13 @@
+//
+//  ZZGTwoTabsView.h
+//  ZZGFilterView
+//
+//  Created by dito on 16/7/14.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZGTwoTabsView : UIView
+
+@end
---
+++ ZZGFilterView/FilterView/ZZGTwoTabsView.m
@@ -0,0 +1,21 @@
+//
+//  ZZGTwoTabsView.m
+//  ZZGFilterView
+//
+//  Created by dito on 16/7/14.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import "ZZGTwoTabsView.h"
+
+@implementation ZZGTwoTabsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
